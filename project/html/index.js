30242667348f1bcc30b6369d55d80f14a40e3ee9
Sanjaikumaran <115868289+Sanjaikumaran@users.noreply.github.com>
2023-01-21 03:52:33 -0800

    Delete index.js

--- project/html/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(
-    function () {
-        $(".option").click(
-            function (event) {
-                $(this).addClass("active").siblings().removeClass("active");
-            }
-        );
-    });
